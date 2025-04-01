c3b71721994fbf4131b9cea985e6f2d22f2473c2
Jason MacLeod <jason.d.macleod@gmail.com>
2025-03-31 18:20:23 -0700

    test: update test name

--- tests/unit/test_aws_cdk_dcv_gw_sm_without_pipelines_stack.py
+++ tests/unit/test_aws_cdk_dcv_gw_sm_without_pipelines_stack.py
@@ -1,12 +1,12 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from aws_cdk_nice_dcv_linux.aws_cdk_nice_dcv_linux_stack import DcvInfra
+from stacks.dcv_gw_sm_infra import DcvInfra
 
 
 def test_sqs_queue_created():
     app = core.App()
-    stack = DcvInfra(app, "aws-cdk-nice-dcv-linux")
+    stack = DcvInfra(app, "aws-cdk-dcv-gw-sm-without-pipelines")
     template = assertions.Template.from_stack(stack)
 
     template.has_resource_properties("AWS::SQS::Queue", {"VisibilityTimeout": 300})
@@ -14,7 +14,7 @@ def test_sqs_queue_created():
 
 def test_sns_topic_created():
     app = core.App()
-    stack = DcvInfra(app, "aws-cdk-nice-dcv-linux")
+    stack = DcvInfra(app, "aws-cdk-dcv-gw-sm-without-pipelines")
     template = assertions.Template.from_stack(stack)
 
     template.resource_count_is("AWS::SNS::Topic", 1)
